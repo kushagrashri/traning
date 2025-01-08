06e835733bd56aaad54df80ac78ae4a4d14087ab
kushagrashri7 <kushagrashrisharma@gmail.com>
2025-01-08 13:45:17 +0530

    celcius to fahrenheit

---
+++ new3.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class new3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the temperature :");
+        int temp = sc.nextInt();
+        int fah = (temp*9/5) +32;
+        System.out.println("the temperature in fahrenheit is :" + fah);    }
+}
