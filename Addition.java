d841857d0705093a43e83e5886e2cd80b7eb6168
kushagrashri7 <kushagrashrisharma@gmail.com>
2025-01-08 16:13:33 +0530

    addition of two numbers

---
+++ Addition.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Addition{
+	public static void main (String[]arg ){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the vale of a :");
+		int a = sc.nextInt();
+		System.out.println("enter the value of b :");
+		int b= sc.nextInt();
+	System.out.println((a+b));
+}
+}
