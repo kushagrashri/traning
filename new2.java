155d40a8df4de0143a59d4cc8ecab5def2f49cba
kushagrashri7 <kushagrashrisharma@gmail.com>
2025-01-08 13:30:27 +0530

    addition

--- new2.java
+++ new2.java
@@ -1,7 +1,11 @@
+import java.util.*;
 public class new2{
 	public static void main (String[]arg ){
-	int a=1;
-	int b=2;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the vale of a :");
+		int a = sc.nextInt();
+		System.out.println("enter the value of b :");
+		int b= sc.nextInt();
 	System.out.println((a+b));
 }
 }
